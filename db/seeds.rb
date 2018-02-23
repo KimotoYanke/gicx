4bea103a8d1295eef2b14fbe97083f1eb649302d
KimotoYanke <kimotoyanke@gmail.com>
2018-02-23 20:16:31 +0900

    Reset when seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Task.all.destroy_all
+Subject.all.destroy_all
+Homeroom.all.destroy_all
+
 for d in 1...5
     homeroom = Homeroom.create(grade: 1, department: d)
     homeroom.subjects.create(name: '国語 I')
